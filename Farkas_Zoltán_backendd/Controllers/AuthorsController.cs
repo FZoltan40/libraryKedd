270cc54214d6141428819d6493688a9a503c6e3c
zadort <zadort@kkszki.hu>
2024-12-10 14:39:01 +0100

    Library Api kész.

---
+++ Farkas_Zoltán_backendd/Controllers/AuthorsController.cs
@@ -0,0 +1,46 @@
+﻿using Farkas_Zoltán_backendd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Farkas_Zoltán_backendd.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly LibrarydbContext _context;
+
+        public AuthorsController(LibrarydbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("feladat9")]
+        public async Task<ActionResult> Get(string name)
+        {
+            var authors = await _context.Authors.Include(aut => aut.Books).FirstOrDefaultAsync(aut => aut.AuthorName == name);
+
+            if(authors != null)
+            {
+                return Ok(authors);
+            }
+
+            return NotFound();
+           
+        }
+
+        [HttpGet("feladat12")]
+        public async Task<ActionResult> NumOfAuthors()
+        {
+            var num = await _context.Authors.CountAsync();
+
+            if(num >= 0)
+            {
+                return Ok($"A szerzők száma: {num}");
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Farkas_Zoltán_backendd/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using Farkas_Zoltán_backendd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Farkas_Zoltán_backendd.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly LibrarydbContext _context;
+
+        public CategoriesController(LibrarydbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("feladat11")]
+        public async Task<ActionResult> Get() 
+        {
+            var categories = await _context.Categories.Include(ctg=> ctg.Books).ToListAsync();
+
+            if(categories!= null)
+            {
+                return Ok(categories);
+            }
+
+            Exception e = new();
+            return BadRequest(e.Message);
+        }
+
+    }
+}
